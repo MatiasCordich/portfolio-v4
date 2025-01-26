d920063b014ec654ecfdac869d9dc01850be5174
MatiasCordich <m.scordich@gmail.com>
2025-01-26 01:39:13 -0300

    cambios agregados

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Asegura que las rutas de los recursos sean correctas
-  build: {
-    outDir: 'dist',  // Asegura que la salida de la build sea en la carpeta 'dist'
-  },
-})
+  base: '/',  // Configuración común
+});
