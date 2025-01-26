eb6b96544671fe79a66d34557c6e917cb778a2fe
MatiasCordich <m.scordich@gmail.com>
2025-01-26 01:33:34 -0300

    Agregado vercel.json para manejar rutas en producción

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',  // Asegura que las rutas de los recursos sean correctas
+  build: {
+    outDir: 'dist',  // Asegura que la salida de la build sea en la carpeta 'dist'
+  },
 })
