d2d7d5a2126081a1c13be753b6f6a502ad526d2a
MatiasCordich <m.scordich@gmail.com>
2025-03-24 16:25:31 -0300

    Se cambio la descripcion del Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const actualYear = new Date().getFullYear();
   return (
     <footer className="footer">
-      <p>Portfolio, Version 4.0 (26 Ene 25)</p>
+      <p>Portfolio, Version 4.0</p>
       <p>{actualYear}</p>
     </footer>
   );
